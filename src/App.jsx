2e7d8057a5af58a24d9d2a1f1c48569645825dad
Your Name <you@example.com>
2025-10-27 17:56:05 +0545

    feat:added hamburger for mobile and added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 function App() {
   const [isLoading, setIsLoading] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
@@ -12,8 +15,8 @@ function App() {
           isLoading ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-
-        
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       </div>
     </>
   );
